e8871f132a0250287304af09574ef790c452bf7c
cookies-git <mailtosreesankar@gmail.com>
2022-02-09 20:05:34 +0530

    #1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import Home from './component/Home';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Articlelist from './component/article/Articlelist';
+import Article from './component/article/Article';
+import Error from './component/Error';
+import AddArticle from './component/article/AddArticle';
+import Register from './component/login/Register';
+import Login from './component/login/Login';
+import Navuser from './component/header/Navuser';
+import Profile from './component/about/Profile';
+import UpdateArticle from './component/article/UpdateArticle';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+    <>
+      <Routes>
+        <Route path="/home" element={<Home />} />
+        <Route path="/Navuser" element={<Navuser/>} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/article-list" element={<Articlelist />} />
+        <Route path="/article/:name" element={<Article />} />
+        <Route path="/addarticle" element={<AddArticle />} />
+        <Route path="/" element={<Login />} />
+        <Route path="/register" element={<Register />} /> 
+        <Route path="/:name/update-article" element={<UpdateArticle/>} /> 
+        <Route path="*" element={<Error />} />
+      </Routes>
+    </>
+    </Router>
   );
 }
 
